99e061844a59584347d34e0ca7f5fb181a960487
zhangheng <zhangheng@moguyun.com>
2019-07-31 09:31:53 +0800

    初始化工程

---
+++ src/main/java/com/peter/JavaStudyApplication.java
@@ -0,0 +1,16 @@
+package com.peter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author peter
+ */
+@SpringBootApplication
+public class JavaStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaStudyApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/peter/basic_algorithms/Test.java
@@ -0,0 +1,13 @@
+package com.peter.basic_algorithms;/**
+ * Created by DELL on 2019/7/31.
+ */
+
+/**
+ * @ClassName Test
+ * @Description TODO
+ * @Author DELL
+ * @Date 2019/7/31 9:30
+ * @Version 1.0
+ */
+public class Test {
+}
